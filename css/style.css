f31c1e26575facbf54e0c18fcd0c751f8fb1a875
samyach14-debug <samyach14@gmail.com>
2026-04-29 16:48:37 +0200

    Delete css directory

--- css/style.css
+++
@@ -1,9 +0,0 @@
-const form = document.getElementById("contactForm");
-const formMessage = document.getElementById("formMessage");
-
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  formMessage.textContent = "Bedankt! Je aanvraag is ontvangen.";
-  form.reset();
-});
