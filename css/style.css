c4fa4855f7e89923c21abec5defecfa5685bdacd
samyach14-debug <samyach14@gmail.com>
2026-04-29 16:47:46 +0200

    Create map css
    de style css in een map gezet

---
+++ css/style.css
@@ -0,0 +1,9 @@
+const form = document.getElementById("contactForm");
+const formMessage = document.getElementById("formMessage");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  formMessage.textContent = "Bedankt! Je aanvraag is ontvangen.";
+  form.reset();
+});
